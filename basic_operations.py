890c4490cb6cb51fc7a9d8b876dc99cf3a300294
Madu Ebuka <elijahmadubueze645@gmail.com>
2026-01-15 07:18:00 -0800

    first commit

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+addition = number1 + number2
+subtraction = number1 - number2
+multipication = number1 * number2
+
+
+print(subtraction)
+print(addition)
+print(multipication)
